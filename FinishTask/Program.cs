8d54bb0e51d642b5ab2c00e77d4020336fb23152
Kate Belozertseva <katebelozertseva@gmail.com>
2022-10-12 22:07:24 +0300

    Did all work with task and described it in file.md :)

--- FinishTask/Program.cs
+++ FinishTask/Program.cs
@@ -1,2 +1,66 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// string[] secondVersion = new string[] { "one", "two", "three", "12" }; //Если хотите взять готовый
+
+Console.Clear();
+
+string[] CreateArray()
+{
+    Console.WriteLine($"Хотите указать размер массива? (Y - Да / Другая клавиша - Нет)");
+    string answer = Console.ReadLine()!.ToLower();
+
+    int size = default;
+
+    if (answer == "y")
+    {
+        Console.WriteLine($"Введите количество элементов: ");
+        size = Convert.ToInt32(Console.ReadLine());
+    }
+    else
+    {
+        size = 5;
+    }
+
+    string[] arr = new string[size];
+
+    Console.WriteLine($"Введите элемент: ");
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = Console.ReadLine()!;
+    }
+    return arr;
+}
+
+void PrintArray(string[] arr)
+{
+    Console.WriteLine();
+    Console.ForegroundColor = ConsoleColor.DarkBlue;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i],5}     ");
+    }
+    Console.ResetColor();
+    Console.WriteLine();
+    Console.WriteLine();
+}
+
+string[] CorrectArray(string[] arr)
+{
+    string[] newArr = new string[arr.Length];
+    int j = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i].Length <= 3)
+        {
+            newArr[j] = arr[i];
+            j++;
+        }
+    }
+    return newArr;
+}
+
+
+string[] array = CreateArray();
+PrintArray(array);
+string[] result = CorrectArray(array);
+PrintArray(result);
+
